dd56bbd1a6466e1d8446f329431cc09154975684
jeannemorin <46577907+jeannemorin@users.noreply.github.com>
2022-12-31 18:10:00 +0100

    Reload Ten new jokes working

--- JokeAppV2/app/src/main/java/com/example/jokeappv2/display/ListScreen.kt
+++ JokeAppV2/app/src/main/java/com/example/jokeappv2/display/ListScreen.kt
@@ -4,8 +4,10 @@ import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Text
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.rounded.ArrowBack
+import androidx.compose.material.icons.rounded.ArrowDropDown
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -17,7 +19,19 @@ import com.example.jokeappv2.model.Joke
 @Composable
 fun ListScreen(vm : JokeViewModel, selectedJoke : (Joke) -> Unit) {
 
-    vm.state.jokeData?.let { JokesList(jokes = it, selectedJoke = selectedJoke) }
+    Column() {
+
+
+        IconButton(onClick = { vm.loadJokes() }) {
+            Icon(
+                imageVector = Icons.Rounded.ArrowDropDown,
+                contentDescription = null
+            )
+        }
+
+        Spacer(modifier = Modifier.height(20.dp))
+        vm.state.jokeData?.let { JokesList(jokes = it, selectedJoke = selectedJoke) }
+    }
 }
 
 @Composable
